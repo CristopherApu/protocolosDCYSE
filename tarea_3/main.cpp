54b635c6241140e761ce0643012b920bc74d5b73
CristopherApu <cristopher.almonacid22@gmail.com>
2024-07-04 16:14:56 -0400

    declaramos struct

---
+++ tarea_3/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct ruta{
+    int IP;
+    int TTL;
+    int salto_siguiente;
+};
+
+struct rutas{
+    ruta entrada;
+    ruta salida;
+};
+
+int main (){
+
+    //insertar lo que falta :'c
+
+    return 0;
+}
